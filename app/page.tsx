57f4c2e330c6c30f132ee408e5dfbbdb18196615
haticebabaoglu <haticebaba63@gmail.com>
2024-02-16 19:52:35 +0300

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,21 @@
-import { useState } from "react";
+"use client";
+
+import { FormEvent, useState } from "react";
 export default function Home() {
   const [inputVal, setInputVal] = useState("")
+  const handleSumbit = (event: FormEvent) => {
+    event.preventDefault();
+  }
   return (
    <div>
     <div>
       <h1>Enter Your Name</h1>
-      <form>
+      <form onSubmit={handleSumbit}>
         <input type="text" placeholder="Type your name"/>
-        <button type="submit">predict data</button>
+        <button type="submit"
+        value={inputVal}
+        onChange={(e) => setInputVal(e.target.value)}
+        >predict data</button>
       </form>
     </div>
    </div>
