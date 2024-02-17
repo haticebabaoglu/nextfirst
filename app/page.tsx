af1811c7531638f6d8cba7940e37ea05640308dd
haticebabaoglu <haticebaba63@gmail.com>
2024-02-17 23:42:50 +0300

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,37 @@
 "use client";
-
+import { useRouter } from "next/navigation";
 import { FormEvent, useState } from "react";
+
 export default function Home() {
-  const [inputVal, setInputVal] = useState("")
-  const handleSumbit = (event: FormEvent) => {
+  const [value, setValue] = useState("");
+  const { push } = useRouter();
+
+  const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-  }
+    push(`/prediction/${value}`);
+  };
   return (
-   <div>
-    <div>
-      <h1>Enter Your Name</h1>
-      <form onSubmit={handleSumbit}>
-        <input type="text" placeholder="Type your name"/>
-        <button type="submit"
-        value={inputVal}
-        onChange={(e) => setInputVal(e.target.value)}
-        >predict data</button>
-      </form>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="p-4 shadow-md bg-white rounded-md">
+        <h1 className="text-2xl font-semibold mb-4 text-black">
+          Enter Your Name
+        </h1>
+        <form onSubmit={handleSubmit} className="space-y-3">
+          <input
+            type="text"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            className="w-full p-2 border border-gray-300 rounded text-black"
+            placeholder="Type something..."
+          />
+          <button
+            type="submit"
+            className="w-full py-2 px-4 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
-   </div>
   );
 }
--- app/prediction/[name]/page.tsx
+++ app/prediction/[name]/page.tsx
@@ -1,7 +1,11 @@
-export default function Page() {
+interface Params{
+  params: { name: string};
+}
+
+export default function Page({params }: Params) {
     return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        name
+        {params.name}
   
       </main>
     );
