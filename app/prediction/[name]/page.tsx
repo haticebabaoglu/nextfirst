401384efa6dfa5e32d24fd4f6756cf3fac5f8caf
haticebabaoglu <haticebaba63@gmail.com>
2024-02-18 21:43:55 +0300

    api

--- app/prediction/[name]/page.tsx
+++ app/prediction/[name]/page.tsx
@@ -1,8 +1,26 @@
+const getPredictedAge = async (name : string) =>{
+  const res = await fetch(`https://api.agify.io/?name=${name}`)
+  return res.json()
+}
+
+const getPredictedGender = async (name : string) =>{
+  const res = await fetch(`https://api.genderize.io/?name=${name}`)
+  return res.json()
+}
+
+const getPredictedCountry = async (name : string) =>{
+  const res = await fetch(`https://api.nationalize.io/?name=${name}`)
+  return res.json()
+}
+
 interface Params{
   params: { name: string};
 }
 
-export default function Page({params }: Params) {
+export default async function Page({params }: Params) {
+    const ageData = getPredictedAge(params.name)
+    const contryData = getPredictedCountry(params.name)
+    const genderData = getPredictedGender(params.name)
     return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         {params.name}
